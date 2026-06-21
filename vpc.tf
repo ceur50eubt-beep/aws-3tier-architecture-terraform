940029cfda56c9a5f669bbd3fa6dd6422d4b143a
ceur50eubt-beep <ceur50eubt@gmail.com>
2026-06-21 14:38:47 +0900

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "3tier-vpc"
+  }
+}
+
+resource "aws_subnet" "public_1a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-northeast-1a"
+
+  tags = {
+    Name = "3tier-public-1a"
+  }
+}
